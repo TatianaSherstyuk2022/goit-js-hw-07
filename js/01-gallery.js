91e98adfa396a30c8bbc698521c18bd0cb7e483e
TatianaSherstyuk2022 <grinkevichtatiana@gmail.com>
2022-12-26 23:08:07 +0200

    add #1,2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,65 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const container = document.querySelector(".js-gallery");
+// const galleryItemEl = document.querySelector('.gallery__link')
+
+function createMarkup(arr) {
+    const markup = arr.map(({ preview, original, description }) => {
+        return `
+        <div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+            />
+            </a>
+        </div>
+        `
+    }).join('');
+    return markup;
+}
+
+container.insertAdjacentHTML('beforeend', createMarkup(galleryItems));
+
+
+const onGalleryItemElClick = event => {
+    event.preventDefault();
+    const { target } = event;
+    // console.log(target.dataset.source);
+
+    if (target.nodeName !== 'A') {
+    return;
+  }
+
+const galleryBigItemEl = basicLightbox.create(`
+    <img src="${target.dataset.source}" width="800" height="600">
+`)
+
+galleryBigItemEl.show()
+}
+
+container.addEventListener('click', onGalleryItemElClick);
+
+
+
+// const onModalOpenImgClick = event => {
+// const instance = basicLightbox.create(`
+//     <img src="${original}" width="800" height="600">
+// `)
+
+// instance.show()
+// }
+
+// container.addEventListener('click', onModalOpenImgClick)
+// import * as basicLightbox from 'basiclightbox'
+
+// const instance = basicLightbox.create(`
+//     <img src="${original}" width="800" height="600">
+// `)
+
+// instance.show()
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const container = document.querySelector(".js-gallery");
+// const galleryItemEl = document.querySelector('.gallery__link')
+
+function createMarkup(arr) {
+  const markup = arr
+    .map(({ preview, original, description }) => {
+      return `
+        <a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+        `;
+    })
+    .join("");
+  return markup;
+}
+
+container.insertAdjacentHTML("beforeend", createMarkup(galleryItems));
+
+var lightbox = new SimpleLightbox('.gallery a', { overlay: '2500ms' });
